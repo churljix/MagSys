a57d5217dee85a14f58baefbf8cce71161a29f4a
churljix <dzeina13@inbox.lv>
2014-04-14 22:56:12 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MagSys::Application.config.secret_key_base = 'aa489c4e423bfaee48297e746ce204d315922886bfd45b0d7481202e760278595207fbc7a99fb0df60f3406db6ac06f954a027f642d9908eecb051378708349d'
